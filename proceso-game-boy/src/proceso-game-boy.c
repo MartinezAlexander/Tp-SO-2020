c370c0076d55515e0e26f5b34cb6df44c13372cf
Alexander Cristian Martinez <alexandercristianmartinez@gmail.com>
2020-05-05 20:54:42 -0300

    Corrijo el makefile de la biblioteca, agrego una prueba de conexion entre el proceso game boy y el proceso team

--- proceso-game-boy/src/proceso-game-boy.c
+++ proceso-game-boy/src/proceso-game-boy.c
@@ -11,7 +11,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <mensajes/client.h>
+#include <mensajes/mensajes.h>
+#include <mensajes/appeared_pokemon.h>
 
 int main(void) {
+	//Prueba de conexion entre proceso team y proceso game boy
+
+	//Creo un socket para conectarme al proceso team
+	int socket1 = crear_conexion("127.0.0.2","5002");
+
+	//Envio el mensaje t_appeared_pokemon al proceso team
+	t_appeared_pokemon* appeared_mensaje1 = appeared_pokemon_create("pikachu",2,3);
+	enviar_mensaje((void*)appeared_mensaje1,APPEARED_POKEMON,socket1);
+
+	//Cierro el socket utilizaado
+	liberar_conexion(socket1);
+
+	int socket2 = crear_conexion("127.0.0.2","5002");
+	t_appeared_pokemon* appeared_mensaje2 = appeared_pokemon_create("messi",1,1);
+	enviar_mensaje((void*)appeared_mensaje2,APPEARED_POKEMON,socket2);
+	liberar_conexion(socket2);
+
+	int socket3 = crear_conexion("127.0.0.2","5002");
+	t_appeared_pokemon* appeared_mensaje3 = appeared_pokemon_create("pikachu",5,2);
+	enviar_mensaje((void*)appeared_mensaje3,APPEARED_POKEMON,socket3);
+	liberar_conexion(socket3);
+
+	int socket4 = crear_conexion("127.0.0.2","5002");
+	t_appeared_pokemon* appeared_mensaje4 = appeared_pokemon_create("charmander",0,4);
+	enviar_mensaje((void*)appeared_mensaje4,APPEARED_POKEMON,socket4);
+	liberar_conexion(socket4);
+
 	return EXIT_SUCCESS;
 }
