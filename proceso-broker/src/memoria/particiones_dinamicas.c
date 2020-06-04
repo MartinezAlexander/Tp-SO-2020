25982fd121c160380089b1fc87ada45f1fbf20f0
Alexander Cristian Martinez <alexandercristianmartinez@gmail.com>
2020-06-04 17:28:29 -0300

    Agrego funciones para crear, manejar y controlar las particiones dinamicas

--- proceso-broker/src/memoria/particiones_dinamicas.c
+++ proceso-broker/src/memoria/particiones_dinamicas.c
@@ -1,2 +1,60 @@
 #include "particiones_dinamicas.h"
 
+t_particion* particion_create(int base, int limite, int libre){
+	t_particion* nueva_particion = malloc(sizeof(t_particion));
+	nueva_particion->base = base;
+	nueva_particion->limite = limite;
+	nueva_particion->libre = libre;
+	nueva_particion->lru = time(NULL);
+	return nueva_particion;
+}
+
+int particion_esta_libre(t_particion* particion){
+	return particion->libre;
+}
+
+t_particion* particion_combinar(t_particion* particion1, t_particion* particion2){
+	int nuevo_limite = particion1->limite + particion2->limite;
+	int nueva_base;
+
+	if(particion1->base <= particion2->base){
+		nueva_base = particion1->base;
+	}else{
+		nueva_base = particion2->base;
+	}
+
+	return particion_create(nueva_base,nuevo_limite,1);
+
+	//TODO liberar memoria de las dos particiones que llegan por parametro
+}
+
+int particion_puede_guardarlo(t_particion* particion, int tamanio_a_guardar){
+	return tamanio_a_guardar <= particion->limite;
+}
+
+int particion_justa(t_particion* particion, int tamanio_a_guardar){
+	return particion->limite == tamanio_a_guardar;
+}
+
+t_particion* particion_ocuparla(t_particion* particion_libre, int tamanio_a_ocupar){
+	t_particion* nueva_particion_libre;
+	if(particion_justa(particion_libre,tamanio_a_ocupar)){
+		nueva_particion_libre = NULL;
+		particion_libre->libre = 0;
+		particion_libre->lru = time(NULL);
+		//TODO Guardar en cache
+	}else{
+		particion_libre->libre = 0;
+		int nuevo_limite_libre = particion_libre->limite - tamanio_a_ocupar;
+		particion_libre->limite = tamanio_a_ocupar;
+		particion_libre->lru = time(NULL);
+		nueva_particion_libre = particion_create(particion_libre->limite+1,nuevo_limite_libre,1);
+		//TODO Guardar en cache
+	}
+
+	return nueva_particion_libre;
+}
+
+void particion_destroy(t_particion* particion){
+	free(particion);
+}
--- proceso-broker/src/memoria/particiones_dinamicas.h
+++ proceso-broker/src/memoria/particiones_dinamicas.h
@@ -2,8 +2,58 @@
 #ifndef MEMORIA_PARTICIONES_DINAMICAS_H_
 #define MEMORIA_PARTICIONES_DINAMICAS_H_
 
+#include <stdlib.h>
+#include <time.h>
 
+typedef struct{
+	int base;
+	int limite;
+	int libre;
+	time_t lru;
+	//TODO podria guardar la cola para agilizar la busqueda
+}t_particion;
 
+/*
+particion_create crea una nueva particion si se le indica la base y el limite
+guardando la hora en la que se creo dicha particion
+*/
+t_particion* particion_create(int base, int limite, int libre);
 
+/*
+particion_esta_libre devuelve verdadero si la particion puede guardar bytes y
+falso si ya esta ocupada
+*/
+int particion_esta_libre(t_particion* particion);
+
+/*
+particion_combinar crea una unica particion a partir de dos particiones
+*/
+t_particion* particion_combinar(t_particion* particion1, t_particion* particion2);
+
+/*
+particion_puede_guardarlo devuelve verdadero si la cantidad de bytes que se desean
+guardar es menor o igual al espacio libre de la particion
+*/
+int particion_puede_guardarlo(t_particion* particion, int tamanio_a_guardar);
+
+/*
+particion_justa devuelve verdadero si la particion tiene el tamaño justo para guardar
+la cantidad de bytes indicadas por parametro y falso si es mayor o menor
+*/
+int particion_justa(t_particion* particion, int tamanio_a_guardar);
+
+/*
+particion_ocuparla recibe una particion libre y la ocupa, luego de ocuparla crea la
+nueva particion libre y la retorna. En caso de que el tamanio a ocupar sea igual que
+el tamanio libre devuelve NULL indicando que no se creo ninguna particion libre nueva.
+En caso de que el tamanio a ocupar se menor que el libre retorna una nueva particion
+libre y modifica la que pasa por parametro dejandola ocupada
+*/
+t_particion* particion_ocuparla(t_particion* particion_libre, int tamanio_a_ocupar);
+
+/*
+particion_destroy liberar la memoria usada al crear la particion
+*/
+void particion_destroy(t_particion* particion);
 
 #endif /* MEMORIA_PARTICIONES_DINAMICAS_H_ */
