9484ed8a325fc2319f488b93abf01a81ec404012
Alexander Cristian Martinez <alexandercristianmartinez@gmail.com>
2020-06-12 22:56:29 -0300

    Agrego el gameboy para realizar las pruebas

---
+++ gameboy/src/logger.h
@@ -0,0 +1,26 @@
+/*
+ * logger.h
+ *
+ *  Created on: 7 jun. 2020
+ *      Author: utnso
+ */
+
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <mensajes/mensajes.h>
+#include <mensajes/suscripcion.h>
+
+t_log* logger;
+
+void iniciar_logger(char* path);
+void loggear_info(char* info);
+void loggear_conexion(t_proceso id_proceso);
+void loggear_suscripcion(op_code id_cola);
+void loggear_nuevo_mensaje(op_code id_cola);
+
+char* proceso_to_string(t_proceso id_proceso);
+#endif /* LOGGER_H_ */
